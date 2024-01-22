e8ecd61f4643d9f70f0e6703bcf77f09968041a7
techeo <tegaecheo@gmail.com>
2024-01-21 17:47:11 -0800

    implementation of operations

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top element of the stack
+ * @head: stack head
+ * @counter: line number
+ * Return: no return value
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
