8a1ceabbc2267f0fc6351748296d4b7e66e9b0af
techeo <tegaecheo@gmail.com>
2024-01-21 18:07:27 -0800

    implemented some operations

---
+++ queue.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * f_queue - Transforms the stack into a queue
+ * @head: pointer to stack head
+ * @counter: line number
+ * Return: no return value
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - adds a node to the tail of the stack
+ * @n: new value
+ * @head: pointer to head of the stack
+ * Return: no return value
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
