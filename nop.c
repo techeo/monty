a4331c83be3c4fd5a39101e4c91d0db6db03eccc
techeo <tegaecheo@gmail.com>
2024-01-21 17:57:30 -0800

    implemented some operations

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop - does nothing
+  *@head: stack head
+  *@counter: line number
+  *Return: no return value
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
